e9509c1fe4c5618a77b7ead8a95b9490f8885287
lw000 <373102227@qq.com>
2017-07-06 15:50:49 +0800

    Signed-off-by: lw000 <373102227@qq.com> 更新

---
+++ project/business/NetHead.cpp
@@ -0,0 +1,32 @@
+﻿#include "NetHead.h"
+
+#include <time.h>
+
+#ifdef _WIN32
+#include <winsock.h>
+#else
+#include <arpa/inet.h>
+#endif
+
+namespace LW 
+{
+	tagNetHead::tagNetHead()
+	{
+		memset(this, 0x00, sizeof(tagNetHead));
+	}
+
+	void tagNetHead::debug()
+	{
+# if defined(_DEBUG) || defined(DEBUG)
+		char hhmmss[9];  /* HH:MM:SS\0 */
+		time_t t;
+		t = (time_t)ntohl(create_time);
+		strftime(hhmmss, sizeof(hhmmss), "%T", localtime(&t));
+		printf("size = %d, cmd = %d, u_reserve = %d, create_time = %s \n",
+			size,
+			cmd,
+			reserve,
+			hhmmss);
+# endif
+	}
+}
--- project/business/NetHead.h
+++ project/business/NetHead.h
@@ -1,46 +1,24 @@
 ﻿#ifndef __NetHead_H_
 #define __NetHead_H_
 
-#include <time.h>
-#ifdef _WIN32
-#include <winsock.h>
-#else
-#include <arpa/inet.h>
-#endif
-
 #include "base_type.h"
 
 namespace LW 
 {
 #pragma  pack(1)
-	// network packet header
 	typedef struct tagNetHead
 	{		
-		lw_int32						size;				// 数据包大小
-		lw_int32						cmd;				// 指令
-		lw_uint32						create_time;		// 发送时间
-		lw_int32						reserve;			// 保留字段
-	public:
-		tagNetHead()
-		{
-			memset(this, 0x00, sizeof(tagNetHead));
-		}
-	public:
-		void debug()
-		{
-# if defined(_DEBUG) || defined(DEBUG)
+		lw_int32	size;			// 数据包大小
+		lw_int32	cmd;			// 指令
+		lw_uint32	create_time;	// 发送时间
+		lw_int32	reserve;		// 保留字段
 
-			char hhmmss[9];  /* HH:MM:SS\0 */
-			time_t t;
-			t = (time_t)ntohl(create_time);
-			strftime(hhmmss, sizeof(hhmmss), "%T", localtime(&t));
-			printf("size = %d, cmd = %d, u_reserve = %d, create_time = %s \n",
-				size,
-				cmd,
-				reserve,
-				hhmmss);
-# endif 
-		}
+	public:
+		tagNetHead();
+
+	public:
+		void debug();
+
 	}  NetHead;
 
 #pragma pack()
